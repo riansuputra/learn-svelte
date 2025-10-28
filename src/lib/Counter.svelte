a5815da3ab03af676284df0b28957ba8f7d42662
Yansu <ianputra935@gmail.com>
2025-10-28 10:44:11 +0800

    update

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,15 @@
+<script>
+    let count = $state(0);
+    function increment() {
+        count++;
+    }
+
+    function decrement() {
+        count--;
+    }
+</script>
+
+<h1>Counter : {count}</h1>
+
+<button onclick={increment}> increment </button>
+<button onclick={decrement}> decrement </button>
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        hello: "hello.html",
+        counter: "counter.html",
+      },
+    },
+  },
+});
